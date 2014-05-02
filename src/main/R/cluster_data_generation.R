4439a901b2ca6ec5f4d0c5e6cde458b6837f9f8f
Andrea Iacono <andrea.iacono.nl@gmail.com>
2014-05-02 16:35:36 +0200

    added generator of points for cluster sample

---
+++ src/main/R/cluster_data_generation.R
@@ -0,0 +1,15 @@
+N <- 100
+x <- rnorm(N) +1
+y <- rnorm(N) +1
+dat <-data.frame(x, y)
+
+x <- rnorm(N) +5
+y <- rnorm(N) +1
+dat <- rbind(dat, data.frame(x, y))
+
+x <- rnorm(N) +1
+y <- rnorm(N) +5
+dat <- rbind(dat, data.frame(x, y))
+
+plot(dat)
+#write.table(dat, file="points.dat")
