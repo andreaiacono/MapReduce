df1a642be0b6b1f9cbd2cf928c7648c3a5ed98b4
Andrea Iacono <andrea.iacono.nl@gmail.com>
2014-05-03 22:33:19 +0200

    updated data and R code for cluster generation

--- src/main/R/cluster_data_generation.R
+++ src/main/R/cluster_data_generation.R
@@ -1,15 +1,12 @@
 N <- 100
-x <- rnorm(N) +1
-y <- rnorm(N) +1
+x <- rnorm(N) +1; y <- rnorm(N) +1
 dat <-data.frame(x, y)
 
-x <- rnorm(N) +5
-y <- rnorm(N) +1
+x <- rnorm(N) +5; y <- rnorm(N) +1
 dat <- rbind(dat, data.frame(x, y))
 
-x <- rnorm(N) +1
-y <- rnorm(N) +5
+x <- rnorm(N) +1; y <- rnorm(N) +5
 dat <- rbind(dat, data.frame(x, y))
 
 plot(dat)
-#write.table(dat, file="points.dat")
+write.table(dat, file="points.dat")
